41f4b242d902b75099fd53fc6652057a289337d4
Raees Azam Shaikh <shaikhraishazam@gmail.com>
2024-04-17 00:06:01 +0530

    this is custom exception file will raise error during run time execution

--- src/exception.py
+++ src/exception.py
@@ -35,10 +35,10 @@ class CustomException(Exception):
 
 
 
-if __name__ == '__main__':
-    try:
-        a=1/0
+# if __name__ == '__main__':
+#     try:
+#         a=1/0
 
-    except Exception as e:
-        logging.info(e)
-        raise CustomException(e,sys)
+#     except Exception as e:
+#         logging.info(e)
+#         raise CustomException(e,sys)
