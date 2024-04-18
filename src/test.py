b6b5cbc0e8d2fba9369b665ddfc0ffb70f4d1d05
Raees Azam Shaikh <shaikhraishazam@gmail.com>
2024-04-18 13:49:34 +0530

    this is test file

--- src/test.py
+++ src/test.py
@@ -6,6 +6,7 @@ from src.logger import logging
 from src.exception import CustomException
 from src.utils import read_data_sql
 from src.components.data_ingestion import  DataIngestion,DataIngestionConfig
+from src.components.data_transformation import  DataTransformation,DataTransformationConfig
 
 def main():
     # Call the read_data_sql function to load the DataFrame
@@ -13,7 +14,11 @@ def main():
 
     #creating an object of DataIngestion class
     di = DataIngestion()
-    di.initiate_data_ingestion()
+    raw_path,train_path,test_path = di.initiate_data_ingestion()
+
+    #creating an object data transformation
+    dt = DataTransformation()
+    print(dt.initiate_data_transformation(raw_path,train_path,test_path))
     
     # Print the DataFrame to check if it loaded correctly
     #print(df)
